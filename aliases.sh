c5afc1cd9e464dacfd47c29003afd063abf89f50
Geofrey <sgeofrey@pakasa.io>
2024-12-05 17:34:33 -0800

    2024-12-05 17:33:25: save changes

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
 alias ws="hugo server -D -O --gc --noHTTPCache"
 alias np="hugo new content -k post -c content/posts"
-alias sv="!gitsave() { git commit -am \"$(date '+%Y-%m-%d %H:%M:%S'): save changes\"; }; gitsave"
-alias up="!gitpush() { git push origin $(git rev-parse --abbrev-ref HEAD); }; gitpush"
+alias sv="gitsave() { git commit -am \"$(date '+%Y-%m-%d %H:%M:%S'): save changes\"; }; gitsave"
+alias up="gitpush() { git push origin $(git rev-parse --abbrev-ref HEAD); }; gitpush"
 alias sync="git pull"
 
