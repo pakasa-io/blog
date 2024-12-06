6a1819504d48f1a70a705f3c12fd2dbe17822276
Geofrey <sgeofrey@pakasa.io>
2024-12-05 17:45:29 -0800

    2024-12-05 17:45:19: save changes

--- aliases.sh
+++ aliases.sh
@@ -3,4 +3,3 @@ alias np="hugo new content -k post -c content/posts"
 alias sv="source $HOME/.bashrc && git commit -am \"$(date '+%Y-%m-%d %H:%M:%S'): save changes\""
 alias up="source $HOME/.bashrc && git push origin $(git rev-parse --abbrev-ref HEAD)"
 alias sync="git pull"
-
